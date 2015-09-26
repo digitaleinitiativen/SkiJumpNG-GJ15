f273e6fe319e2256ab367137bbfb070f97e6f00c
Erfan Ebrahimnia <Erfan@John-Snow-will-die.local>
2015-09-26 16:31:11 +0200

    added skijump power

--- js/jumper.js
+++ js/jumper.js
@@ -1,5 +1,6 @@
 SkiJump.Jumper = function(game, x, y, key) {
     this.game = game;
+    this.jumpPower = 0;
 
     Phaser.Sprite.call(this, game, x, y, key, 0);
 };
@@ -8,9 +9,19 @@ SkiJump.Jumper.prototype = Object.create(Phaser.Sprite.prototype);
 SkiJump.Jumper.prototype.constructor = SkiJump.Jumper;
 
 SkiJump.Jumper.prototype.create = function() {
+    this.animations.add('stand', [20], 1, false);
     this.anchor.setTo(0.5, 0);
 };
 
 SkiJump.Jumper.prototype.update = function() {
-    
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACE)) {
+        if (
+            this.body.x >= SkiJump.consts.JUMP_AREA_START &&
+            this.body.x <= SkiJump.consts.JUMP_AREA_END
+        ) {
+            this.jumpPower += 5;
+        } else {
+            this.jumpPower -= 5;
+        }
+    }
 };
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
         HEIGHT: 2048
     };
 
+    Phaser.InputHandler = true;
+
     SkiJump = global.SkiJump || {
         consts: consts,
         game: new Phaser.Game(consts.WIDTH / 2, consts.HEIGHT / 2, Phaser.AUTO, 'game')
